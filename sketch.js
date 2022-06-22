6b4f8c6b1678345fa517816a1be4d7f5292deef1
Hebert Luís Gomes Gonçalves <httpshebert@gmail.com>
2022-06-21 21:44:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var back_img, player_img, obstacleImage, fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var gameState = 0, playerCount = 0, player1score = 0, player2score = 0, score = 0;
+var fruits, fruitGroup, obstacle, obstacleGroup;
+var allPlayers, players, player1, player2;
+var database, player, form, game;
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  obstacleImage = loadImage("images/mushroom.png");
+  fruitGroup = new Group();
+}
+
+function setup(){
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+
+   if (gameState === 2) {
+     game.end();
+   }
+}
